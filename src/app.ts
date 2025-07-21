f9fba422b430a71336d8274634f08aee22274665
Ritesh7767 <ritesh776782@gmail.com>
2025-07-21 14:17:54 +0530

    updated home route

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send("hii there")
 })
 
 app.listen(3000, () => console.log("Server is listening at port 3000"))
