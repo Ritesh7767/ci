a6c7c11bcde8eb6e446f2cb72d4f6b50506d4610
Ritesh7767 <ritesh776782@gmail.com>
2025-07-21 14:34:26 +0530

    updated dynamic route

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,10 @@ app.get("/owner", (req, res) => {
     res.send("this is ritesh")
 })
 
-app.get("username:id", (req, res) => {
+app.get("/username:id", (req, res) => {
     const name  = req.params.id
+    console.log(name)
     res.send(name)
-
 })
 
 app.listen(3000, () => console.log("Server is listening at port 3000"))
